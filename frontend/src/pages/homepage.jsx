6aa22df24dda91e686763992f995892628d1c6b9
Niek Geerligs <geerligsniek@gmail.com>
2026-01-07 13:40:40 +0100

    added a courousel

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 import NavBar from '../components/navBar'
 import { ListTile } from '../components/listTile'
 import { Footer } from '../components/footer'
+import { Carousel } from '../components/carousel'
 
-
+const carouselItems = [
+    {
+        image: '/example.png',
+        href: '/',
+        alt: 'Description',
+        caption: 'Title',
+        description: 'Optional description'
+    },
+    {
+        image: 'example.png',
+        href: '/',
+        alt: 'Description 2'
+    }
+];
 
 const Homepage = () => {
     return (
         <div className="w-full min-h-screen flex flex-col bg-gray-100">
             <NavBar />
             <main className="flex flex-1 flex-col items-center justify-center text-center p-4">
-                <h1 className="text-4xl font-bold mb-4">Website in Opbouw👷🏗️🔨</h1>
-                <img
-                    src="/top2000logo.png"
-                    alt="Top2000 Logo"
-                    style={{ width: "200px", height: "auto", marginBottom: "1rem" }}
-                />
-                <p className="text-lg max-w-md">
-                    De Top2000-site wordt op dit moment gebouwd.
-                </p>
-                <p className="text-md mt-4 text-gray-600">🚧Kom later gerust terug!🚧</p>
+                <Carousel items={carouselItems} carouselId="myCarousel" />
                 <ListTile
                     position={1}
                     imagePath="/some/cover.jpg"
