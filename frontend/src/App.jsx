bafaae6aea67f36b44017bb02d2c447869c61c76
Lenn Croes <0351222@student.rocvantwente.nl>
2026-01-14 12:20:41 +0100

    artist page with hard coded data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
-import Song from "./pages/Song.jsx";
+import Song from "./pages/song.jsx";
+import Artist from "./pages/artist.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/Song" element={<Song />} />
+      <Route path="/Artist" element={<Artist />} />
     </Routes>
   );
 }
